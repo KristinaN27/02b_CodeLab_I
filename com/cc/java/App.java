dddc28b5a728f6640df27f4f61d44213fa91bf54
Peter <schmidt.multimedia@gmail.com>
2022-07-21 19:55:08 +0200

    update

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,13 +5,12 @@ public class App {
     public static void main(String[] args) {
     
         Konto konto1 = new Konto(10000);
-            System.out.println("Konto1 = " + konto1.getKontostand() + " €");
-        
         Konto konto2 = new Konto(10000);
-            System.out.println("Konto2 = " + konto2.getKontostand() + " €");
-
         Konto konto3 = new Konto(10000);
-            System.out.println("Konto3 = " + konto3.getKontostand() + " €");  
+
+        System.out.println("Konto1: " + konto1.getKontostand());
+        System.out.println("Konto2: " + konto2.getKontostand());
+        System.out.println("Konto3: " + konto3.getKontostand());  
         
         System.out.println("-----------------------------");
 
@@ -19,9 +18,9 @@ public class App {
         konto2.setKontostand(konto2.getKontostand()*3);
         konto3.setKontostand(konto3.getKontostand()*10);
 
-        System.out.println("Konto1 = " + konto1.getKontostand() + " €");
-        System.out.println("Konto2 = " + konto2.getKontostand() + " €");
-        System.out.println("Konto3 = " + konto3.getKontostand() + " €");
+        System.out.println("Konto1: " + konto1.getKontostand());
+        System.out.println("Konto2: " + konto2.getKontostand());
+        System.out.println("Konto3: " + konto3.getKontostand());  
     }
 }
 
